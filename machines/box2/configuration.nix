4f681e428d7ab4e41014ee3e56b791f1bf5f6d25
Nicolas Van Damme <nicolas.van.damme2@hotmail.com>
2025-03-23 23:54:15 +0100

    Clean and document

--- machines/box2/configuration.nix
+++ machines/box2/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 let
@@ -15,11 +11,6 @@ in
       ../../common/ssh.nix
       ../../common/powermgmt.nix
     ];
-
-  # Use the systemd-boot EFI boot loader.
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-  #boot.loader.grub.device = "/dev/sda";
 
   boot.loader = {
     systemd-boot.enable = true;
@@ -41,21 +32,6 @@ in
 
   # Set your time zone.
   time.timeZone = "Europe/Brussels";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
 
   services.powerManagement.enable = true;
 
@@ -90,9 +66,6 @@ in
     };
   };
 
-
-  # programs.firefox.enable = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -101,19 +74,6 @@ in
     pkgs.sshfs
     git
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  #services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
