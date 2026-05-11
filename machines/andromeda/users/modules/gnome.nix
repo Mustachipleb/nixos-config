6278e6e644fbe5d35fed2b032629d76fd03a3b32
Nicolas Van Damme <nicolas.van.damme2@hotmail.com>
2026-05-11 15:51:14 +0200

    Update dconf

--- machines/andromeda/users/modules/gnome.nix
+++ machines/andromeda/users/modules/gnome.nix
@@ -34,14 +34,17 @@
         sleep-inactive-ac-type = "suspend";
       };
       "org/gnome/shell/extensions/search-light" = {
-        shortcut-search = [ "<Control>space" ];
+        shortcut-search = [ "<Control>Return" ];
       };
       "org/gnome/shell/extensions/show-desktop-button" = {
         indicator-position = "LEFT";
       };
       "org/gnome/shell/extensions/dash-to-dock" = {
         always-center-icons = false;
+        autohide = true;
+        autohide-in-fullscreen = false;
         background-opacity = 0.0;
+        click-action = "previews";
         custom-theme-shrink = false;
         dash-max-icon-size = 48;
         dock-position = "BOTTOM";
@@ -56,6 +59,10 @@
         preview-size-scale = 0.0;
         show-icons-emblems = true;
         show-show-apps-button = true;
+        shift-click-action = "minimize";
+        shift-middle-click-action = "launch";
+        middle-click-action = "launch";
+        running-indicator-style = "DOTS";
         transparency-mode = "DEFAULT";
         show-mounts = false;
       };
