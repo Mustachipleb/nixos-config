ab5afa649e4c548a7a19c6387e1824b74ca2b54d
Nicolas Van Damme <nicolas.van.damme2@hotmail.com>
2026-07-19 12:58:43 +0200

    chore: Redo mount locations

--- machines/andromeda/modules/mounts.nix
+++ machines/andromeda/modules/mounts.nix
@@ -20,35 +20,14 @@ in
 {
   # System brokey
   # fileSystems."/home/mustachio/triangulum" = mkSshfs "mustachio@192.168.1.10:/home/mustachio";
-  fileSystems."/home/mustachio/pinwheel" = mkSshfs "mustachio@192.168.1.11:/home/mustachio";
-  fileSystems."/home/mustachio/pinwheel_downloads" =
-    mkSshfs "mustachio@192.168.1.11:/mnt/qbittorrent_downloads";
-  fileSystems."/home/mustachio/media" = mkSshfs "mustachio@192.168.1.112:/media";
-  fileSystems."/home/mustachio/backups" = mkSshfs "mustachio@192.168.1.112:/Backups";
   fileSystems."/home/mustachio/mayall" = {
     device = "/dev/disk/by-uuid/11cd9994-37fa-402e-83b9-8321d51b4acc";
     fsType = "ext4";
   };
 
-  # TODO: Leads to emergency mode as the init systemd services can't finish successfully
-  #  systemd.services.ldmtool-create-volume = {
-  #    description = "Create LDM volume";
-  #    wantedBy = [ "specialisation.target" ];
-  #    before = [ "local-fs.target" ];
-  #    after = [ "dev-mapper-ldm_vol_DESKTOP-7CMT1B6-Dg0_Volume1.device" ]; # Optional: wait for physical device if needed
-  #    serviceConfig = {
-  #      Type = "oneshot";
-  #      ExecStart = "${pkgs.ldmtool}/bin/ldmtool create volume 8df9711e-2286-11ee-a0ef-244bfe93153b Volume1";
-  #      RemainAfterExit = true;
-  #    };
-  #  };
-  #
-  #  fileSystems."/home/mustachio/zeta" = {
-  #    # The mapping of the volume is NOT included in the config
-  #    # so, if the mapper does not exist, run:
-  #    # sudo ldmtool create volume 8df9711e-2286-11ee-a0ef-244bfe93153b Volume1
-  #    # Assuming the GUID and volume are still the same, but they should be...
-  #    device = "/dev/mapper/ldm_vol_DESKTOP-7CMT1B6-Dg0_Volume1";
-  #    depends = [ "ldmtool-create-volume.service" ];
-  #  };
+  fileSystems."/mnt/pinwheel" = mkSshfs "mustachio@192.168.1.11:/home/mustachio";
+  fileSystems."/mnt/pinwheel_downloads" = mkSshfs "mustachio@192.168.1.11:/mnt/qbittorrent_downloads";
+  fileSystems."/mnt/media" = mkSshfs "mustachio@192.168.1.112:/media";
+  fileSystems."/mnt/backups" = mkSshfs "mustachio@192.168.1.112:/Backups";
+  fileSystems."/mnt/circinus" = mkSshfs "mustachio@circinus:/home/mustachio";
 }
